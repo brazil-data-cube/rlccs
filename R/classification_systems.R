9b97663dc152f6a347420a0c1e1e71a12434172e
Felipe Carvalho <lipecaso@gmail.com>
2021-02-10 21:05:30 +0000

    Add base code and man

--- R/classification_systems.R
+++ R/classification_systems.R
@@ -1,2 +1,77 @@
-#lccs(link) %>% classification_systems() %>% classes() %>%
-#  post_request()
+#' @title ...
+#' @description ...
+#'
+#' @param q ...
+#' @param system_id ...
+#' @param params_list ...
+#'
+#' @return ...
+#'
+#' @export
+classification_systems <- function(q, system_id = NULL, params_list = list()) {
+
+  # check q parameter
+  check_subclass(q, "lccs")
+
+  params <- list()
+
+  subclass <- "classification_systems"
+  if (!is.null(system_id)) {
+
+    if (length(system_id) != 1)
+      .error("Parameter `collection_id` must be a single value.")
+
+    params[["system_id"]] <- system_id
+
+    subclass <- "classification_systems_id"
+  }
+
+  params <- utils::modifyList(params, params_list)
+
+
+  RLCCSQuery(base_url = q$base_url,
+             params = utils::modifyList(q$params, params),
+             subclass = subclass)
+}
+
+#' @export
+endpoint.classification_systems <- function(q) {
+  return("/classification_systems")
+}
+
+before_request.classification_systems <- function(q) {
+
+  check_query_verb(q, verbs = c("GET", "POST"))
+
+  return(q)
+}
+
+after_response.classification_systems <- function(q, res) {
+  content <- content_response(res, c("200", "201", "400", "404", "500"),
+                              "application/json")
+
+  RLCCSDocument(content = content, q = q, subclass = "ClassificationSystem")
+}
+
+#' @export
+endpoint.classification_systems_id <- function(q) {
+  return(paste("/classification_systems", q$params[["system_id"]], sep = "/"))
+}
+
+#' @export
+before_request.classification_systems_id <- function(q) {
+
+  check_query_verb(q, verbs = c("GET", "PUT", "DELETE"))
+
+  q <- omit_query_params(q, names = "system_id")
+
+  return(q)
+}
+
+#' @export
+after_response.classification_systems_id <- function(q, res) {
+  content <- content_response(res, c("200", "400", "404", "500"),
+                              "application/json")
+
+  RLCCSDocument(content = content, q = q, subclass = "ClassificationSystem")
+}
---
+++ R/request.R
@@ -0,0 +1,225 @@
+#' @title LCCS API request functions
+#'
+#' @rdname request
+#'
+#' @description The \code{get_request} ...
+#'
+#' The \code{post_request} ...
+#'
+#' The \code{put_request} ...
+#'
+#' The \code{delete_request} ...
+#'
+#' @param q         a \code{RLCCSQuery} object expressing a LCCS query
+#' criteria.
+#'
+#' @param ... ...
+#'
+#' @param encode    a \code{character} informing the request body
+#' Content-Type. Accepted types are \code{'json'} (\code{'application/json'}),
+#' \code{'form'} (\code{'application/x-www-form-urlencoded'}),
+#' and \code{'multipart'} (\code{'multipart/form-data'}). Defaults to
+#' \code{'json'}.
+#'
+#' @return ...
+#'
+#' @export
+get_request <- function(q, ...) {
+
+  # check the object class
+  .check_obj(q, "RLCCSQuery")
+
+  # stamp verb
+  q$verb <- "GET"
+  q$encode <- NULL
+
+  # set endpoint
+  q$endpoint <- endpoint(q)
+
+  # process LCCS object
+  q <- before_request(q)
+
+  # process omitted params
+  q <- .do_omit_query_params(q)
+
+  tryCatch({
+    res <- httr::GET(url = .make_url(q$base_url,
+                                     endpoint = q$endpoint,
+                                     params = q$params), ...)
+  },
+  error = function(e) {
+
+    .error("Request error. %s", e$message)
+  })
+
+  # restore omitted params
+  q <- .undo_omit_query_params(q)
+
+  # process content according to status-code and content-type
+  content <- after_response(q, res = res)
+
+  return(content)
+}
+
+#' @rdname request
+#' @export
+post_request <- function(q, ..., encode = c("json", "multipart", "form")) {
+
+  # check the object class
+  .check_obj(q, "RLCCSQuery")
+
+  # check request settings
+  httr_encode <- c("json", "multipart", "form")
+  encode <- encode[[1]]
+  if (!encode %in% httr_encode)
+    .error("Invalid body `encode` '%s'. Allowed `econde` are %s.",
+           encode, paste0("'", httr_encode, "'", collapse = ", "))
+
+  # stamp verb
+  q$verb <- "POST"
+  q$encode <- encode
+
+  # set endpoint
+  q$endpoint <- endpoint(q)
+
+  # process LCCS object
+  q <- before_request(q)
+
+  # process omitted params
+  q <- .do_omit_query_params(q)
+
+  tryCatch({
+    res <- httr::POST(url = .make_url(q$base_url, endpoint = q$endpoint), ...,
+                      body = q$params, encode = q$encode)
+  },
+  error = function(e) {
+    .error("Request error. %s", e$message)
+  })
+
+  # restore omitted params
+  q <- .undo_omit_query_params(q)
+
+  # process content according to status-code and content-type
+  content <- after_response(q, res = res)
+
+  return(content)
+}
+
+#' @rdname request
+#' @export
+put_request <- function(q, ..., encode = c("json", "multipart", "form")) {
+
+  # check the object class
+  .check_obj(q, "RLCCSQuery")
+
+  # check request settings
+  httr_encode <- c("json", "multipart", "form")
+  encode <- encode[[1]]
+  if (!encode %in% httr_encode)
+    .error("Invalid body `encode` '%s'. Allowed `econde` are %s.",
+           encode, paste0("'", httr_encode, "'", collapse = ", "))
+
+  # stamp verb
+  q$verb <- "PUT"
+  q$encode <- encode
+
+  # set endpoint
+  q$endpoint <- endpoint(q)
+
+  # process LCCS object
+  q <- before_request(q)
+
+  # process omitted params
+  q <- .do_omit_query_params(q)
+
+  tryCatch({
+    res <- httr::PUT(url = .make_url(q$base_url, endpoint = q$endpoint), ...,
+                      body = q$params, encode = q$encode)
+  },
+  error = function(e) {
+    .error("Request error. %s", e$message)
+  })
+
+  # restore omitted params
+  q <- .undo_omit_query_params(q)
+
+  # process content according to status-code and content-type
+  content <- after_response(q, res = res)
+
+  return(content)
+}
+
+#' @rdname request
+#'@export
+delete_request <- function(q, ...) {
+
+  # check the object class
+  .check_obj(q, "RLCCSQuery")
+
+  # stamp verb
+  q$verb <- "DELETE"
+  q$encode <- NULL
+
+  # set endpoint
+  q$endpoint <- endpoint(q)
+
+  # process LCCS object
+  q <- before_request(q)
+
+  # process omitted params
+  q <- .do_omit_query_params(q)
+
+  tryCatch({
+    res <- httr::DELETE(url = .make_url(q$base_url,
+                                     endpoint = q$endpoint,
+                                     params = q$params), ...)
+  },
+  error = function(e) {
+
+    .error("Request error. %s", e$message)
+  })
+
+  # restore omitted params
+  q <- .undo_omit_query_params(q)
+
+  # process content according to status-code and content-type
+  content <- after_response(q, res = res)
+
+  return(content)
+}
+
+
+#' @describeIn extensions
+#' The \code{.do_omit_query_params()} Function to make the omission of the
+#'  parameters that were omitted in function \code{omit_query_params()}.
+#'
+#' @param q       a \code{RLCCSQuery} object.
+#'
+#' @noRd
+.do_omit_query_params <- function(q) {
+
+  if (is.character(q$omitted)) {
+
+    to_omit <- names(q$param) %in% q$omitted
+    if (length(to_omit) > 0) {
+      q$omitted <- q$params[to_omit]
+      q$params[to_omit] <- NULL
+    }
+  }
+  q
+}
+
+#' @describeIn extensions
+#' The \code{.undo_omit_query_params()} function to undo the omission of
+#'  parameters that were omitted in function \code{omit_query_params()}.
+#'
+#' @param q       a \code{RLCCSQuery} object.
+#'
+#' @noRd
+.undo_omit_query_params <- function(q) {
+
+  if (is.list(q$omitted))
+    q$params <- utils::modifyList(q$params, q$omitted)
+  q$omitted <- NULL
+  q
+}
